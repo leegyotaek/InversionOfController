2adfa565173ff26f38dc20339b85c2755a4e6f4c
leegyotaek <leegyotaek@gmail.com>
2020-04-05 15:44:17 +0900

    스프링 java bean 가져오기 예시 - xml 방식 - annotation 방식 - 자바 코드 방식

--- src/com/hellotaek/springdemo/JapaneseTranslator.java
+++ src/com/hellotaek/springdemo/JapaneseTranslator.java
@@ -1,5 +1,8 @@
 package com.hellotaek.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component(value = "jpTranslator")
     public class JapaneseTranslator implements Translator {
         @Override
         public String sayHello() {
--- src/com/hellotaek/springdemo/JavaConfMainApp.java
+++ src/com/hellotaek/springdemo/JavaConfMainApp.java
@@ -1,23 +1,14 @@
 package com.hellotaek.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import sun.tools.java.ClassPath;
 
-import javax.swing.text.AsyncBoxView;
-
-public class MainApp {
+public class JavaConfMainApp {
     public static void main(String[] args) {
 
         // 스프링 설정 파일 가져오기
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        // 스프링 컨테이너에서 빈 얻어오기
-        Translator translator = context.getBean("myTranslator", Translator.class);
-
-        // bean의 메소드 호출
-        System.out.println(translator.sayHello());
-
+        AnnotationConfigApplicationContext context =
+                 new AnnotationConfigApplicationContext(LanConfig.class);
 
         // 어노테이션 방식으로 빈 가져오기
         Translator translator2 = context.getBean("jpTranslator", Translator.class);
